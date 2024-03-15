28e1e34ee76f8ca108b940f596035233ff9ddeab
Paweł Skawicki <pawelskawicki@MacBook-Air-Pawe.local>
2024-03-15 13:44:58 +0100

    git init

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Polub ten film 🔥",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Zasubskrybować kupe 💩",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Zrobic siku",
+    completed: false,
+  },
+];
